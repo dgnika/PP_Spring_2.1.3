1ed52fdabb19bbe5b1cb9c6e242dd0f719eb6bdb
veronika <112850674+dgnika@users.noreply.github.com>
2026-04-07 11:30:04 +0300

    Update AnimalsCage.java

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,24 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
+    @Autowired
+    @Qualifier("dog")
     private Animal animal;
-    private Timer timer;
 
     @Autowired
-    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
-        this.animal = animal;
-        this.timer = timer;
-    }
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
     public Timer getTimer() {
-        return this.timer;
+        return timer;
     }
 }
